2e32a789635646064f607cf8fc8faf0c97ce7a53
Lucca Biagi de Paula Prado - LPR <lucca.prado@iteris.com.br>
2019-01-10 11:05:42 -0200

    commit inicial

---
+++ src/main/java/br/luccaprado/PensadorApi/PensadorApiApplication.java
@@ -0,0 +1,14 @@
+package br.luccaprado.PensadorApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensadorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensadorApiApplication.class, args);
+	}
+
+}
+
