41bdfe9e2c5eb89375379c61547fa919a3d9a391
Lucca Biagi de Paula Prado - LPR <lucca.prado@iteris.com.br>
2019-01-14 11:40:10 -0200

    otimizando imports

--- src/main/java/br/luccaprado/PensadorApi/services/BaseService.java
+++ src/main/java/br/luccaprado/PensadorApi/services/BaseService.java
@@ -1,9 +1,8 @@
 package br.luccaprado.PensadorApi.services;
+
 import br.luccaprado.PensadorApi.model.interfaces.Conecta;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.projection.ProjectionFactory;
 
 import java.io.IOException;
 
